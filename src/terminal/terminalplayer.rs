a89217d542805e031a1a5bd1691762f888a27979
c0b23092 <c0b23092db@edu.teu.ac.jp>
2025-07-29 12:32:18 +0900

    Update : Version 0.2.1 -> 0.2.2(a) - Cargo.toml : crates.io用に調整 - terminalplayer.rs : 処理の修正

--- src/terminal/terminalplayer.rs
+++ src/terminal/terminalplayer.rs
@@ -2,20 +2,22 @@ use crate::core::player;
 use std::{thread, time::Duration};
 use anyhow::Result;
 
-pub fn one_play(file_name:&str, volume:u8, mut timer:u64) -> Result<()> {
+pub fn one_play(file_name:&str, volume:u8, timer:u64) -> Result<()> {
     let file_path = player::check_and_get_path(file_name)?;
     let (_stream_handle, sink) = player::initialize_soundplayer()?;
-    let duration = player::append_one_track(&sink,file_path)?;
+    let mut duration = player::append_one_track(&sink,file_path)?;
     sink.set_volume(volume.clamp(0,200) as f32 / 100.0);
     sink.play();
-    let (hours,minutes,seconds,_) = player::format_duration(duration);
-    if timer == 0 || duration.as_secs() < timer {
-        timer = duration.as_secs();
-    }
-    println!("{} | {}:{:02}:{:02} | {}:{:02}:{:02}",
+    let (hours,minutes,seconds,millis) = player::format_duration(duration);
+    print!("{} | {}:{:02}:{:02}:{:03} | ",
                 file_path.file_name().unwrap().to_string_lossy().into_owned(),
-                hours, minutes, seconds,
-                timer / 60 / 60, timer / 60 % 60, timer % 60);
-    thread::sleep(Duration::from_secs(timer));
+                hours, minutes, seconds, millis);
+    if timer == 0 || duration.as_secs() < timer {
+        println!("{}:{:02}:{:02}:{:03}",hours,minutes,seconds,millis);
+    }else{
+        duration = Duration::from_secs(timer);
+        println!("{}:{:02}:{:02}",timer / 60 / 60, timer / 60 % 60, timer % 60);
+    }
+    thread::sleep(duration);
     Ok(())
 }
